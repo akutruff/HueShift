c5424f7e459af7e7fc444933d1d98e718cc2d0ba
Andy Kutruff <email@example.com>
2018-05-10 20:00:35 -0400

    bumped version number

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #copy current build
-curl -sSL -o HueShift.zip https://github.com/akutruff/HueShift/releases/download/0.1/HueShift.zip 
+curl -sSL -o HueShift.zip https://github.com/akutruff/HueShift/releases/download/0.2/HueShift.zip 
 unzip HueShift.zip -d /home/pi/HueShift
 
 chown -R pi:pi /home/pi/HueShift
