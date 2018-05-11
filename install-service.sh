647bc88d3cdef10ca6d0dcc94ba117f6b429e475
Andy Kutruff <email@example.com>
2018-05-10 22:15:21 -0400

    another setup file fix

--- install-service.sh
+++ install-service.sh
@@ -1,5 +1,5 @@
 curl -sSL -o /home/pi/hueShift.service https://raw.githubusercontent.com/akutruff/HueShift/master/HueShift/hueShift.service
 
-cp /home/pi/HueShift/hueShift.service /etc/systemd/system
+cp /home/pi/hueShift.service /etc/systemd/system
 systemctl start hueShift.service
 systemctl enable hueShift.service
