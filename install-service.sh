f949283483a89bfabf103057a060522e8ff7b759
Andy Kutruff <email@example.com>
2018-05-10 22:05:41 -0400

    Update service install script

--- install-service.sh
+++ install-service.sh
@@ -1,4 +1,4 @@
-
+curl -sSL -o /home/pi/hueShift.service https://raw.githubusercontent.com/akutruff/HueShift/master/HueShift/hueShift.service
 
 cp /home/pi/HueShift/hueShift.service /etc/systemd/system/hueShift.service
 systemctl start hueShift.service
